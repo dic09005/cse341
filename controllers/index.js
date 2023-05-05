ae5be2da658ea66bcbc06b7391ec44d5b9671f1b
Robin Dickson <dic09005@byui.edu>
2023-05-04 19:02:12 -0600

    commit

--- controllers/index.js
+++ controllers/index.js
@@ -19,4 +19,49 @@ const getObject = async (req, res, next) => {
   });
 };
 
-module.exports = { getData, getObject };
+const createContact = async (req, res, next) => {
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const response = await mongodb.getDb().db('CSE341').collection('contacts').insertOne(contact);
+  if (response.acknowledged) {
+    res.status(201).json(response);
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while creating the contact.');
+  }
+};
+
+const updateContact = async (req, res, next) => {
+  const userId = new ObjectId(req.params.id);
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const response = await mongodb.getDb().db('CSE341').collection('contacts').replaceOne({ _id: userId }, contact);
+  console.log(response);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+  }
+};
+
+const deleteContact = async (req, res, next) => {
+  const userId = new ObjectId(req.params.id);
+  const result = await mongodb.getDb().db('CSE341').collection('contacts').remove({ _id: userId }, true);
+  console.log(response);
+  if (result.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(result.error || 'Error occurred while deleting contact');
+  }
+};
+
+module.exports = { getData, getObject, createContact, updateContact, deleteContact };
--- routes/contacts.js
+++ routes/contacts.js
@@ -7,4 +7,11 @@ router.get('/', myController.getData);
 
 router.get('/:id', myController.getObject);
 
+router.post('/', myController.createContact);
+
+router.put('/:id', myController.updateContact);
+
+router.delete('/:id', myController.deleteContact);
+
+
 module.exports = router;
