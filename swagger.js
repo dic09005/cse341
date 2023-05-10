5c337ac03ff6203e6873097b7508a12c1cf050d2
Robin Dickson <dic09005@byui.edu>
2023-05-10 16:48:00 -0600

    commmit

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: 'My API',
     description: 'Contacts API',
   },
-  host: 'https://dickson341test.onrender.com/',
+  host: 'https://dickson341test.onrender.com',
   schemes: ['http'],
 };
 
