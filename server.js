28dcebc5eb82cffbe8fad4b086eb39634e862efe
Robin Dickson <dic09005@byui.edu>
2023-05-10 18:41:38 -0600

    commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', '*');
+    // res.setHeader('Access-Control-Allow-Headers', '*');
     // res.setHeader('Content-Type', 'application/json');
     res.setHeader('Access-Control-Allow-Methods', '*');
     next();
